79ccbf30bab0d48c2a6f5ea446f4e1d115b3c135
JoeDurand <joe.durand3@gmail.com>
2017-11-13 10:47:04 -0800

    server init commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+// uncomment after placing our favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'faicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')))
+
+var index = require('./routes/index');
+app.use('/', index);
+
+var http = require('http').Server(app);
+var input = require('./routes/input')(http);
+app.use('/input', input);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+http.listen(3000);
