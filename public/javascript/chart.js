77f63318b63b5047b018afbdb0918493edb40e68
ebeneroff <evan.beneroff@gmail.com>
2018-02-24 19:59:20 -0800

    two benchmark comparison, mockups for standard dev, percentile, system information

--- public/javascript/chart.js
+++ public/javascript/chart.js
@@ -1,40 +1,146 @@
 var ctx = document.getElementById("myChart").getContext('2d');
-Chart.defaults.global.defaultFontColor = "#fff";
+Chart.defaults.global.defaultFontColor = "#686a8b";
 
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ["Mount", "Render", "Update", "Destroy", "Create", "Swap"],
         datasets: [{
-            label: 'Amount of Stuff',
+            label: 'Benchmark 1',
             data: [.5, .5, .5, .5, .5, .5],
+            hidden: false,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                // 'rgba(54, 162, 235, 0.2)',
+                // 'rgba(255, 206, 86, 0.2)',
+                // 'rgba(75, 192, 192, 0.2)',
+                // 'rgba(153, 102, 255, 0.2)',
+                // 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
+                'rgba(255, 74, 75, .5)',
             ],
-            borderWidth: 1
-        }]
-    },
+            borderWidth: 2
+        },
+        {
+            label: 'Benchmark 2',
+            data: [.25, .25, .25, .25, .25, .25],
+            hidden: false,
+            backgroundColor: [
+                'rgba(27, 175, 255, .5)',
+                'rgba(27, 175, 255, .5)',
+                'rgba(27, 175, 255, .5)',
+                'rgba(27, 175, 255, .5)',
+                'rgba(27, 175, 255, .5)',
+                'rgba(27, 175, 255, .5)',
+                'rgba(27, 175, 255, .5)',
+                // 'rgba(255, 99, 132, 0.2)',
+                // 'rgba(54, 162, 235, 0.2)',
+                // 'rgba(255, 206, 86, 0.2)',
+                // 'rgba(75, 192, 192, 0.2)',
+                // 'rgba(153, 102, 255, 0.2)',
+                // 'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(27, 175, 255, 1)',
+                'rgba(27, 175, 255, 1)',
+                'rgba(27, 175, 255, 1)',
+                'rgba(27, 175, 255, 1)',
+                'rgba(27, 175, 255, 1)',
+                'rgba(27, 175, 255, 1)',
+            ],
+            borderWidth: 2
+        }
+    ]},
     options: {
+        title: {
+            display: true,
+            text: 'TEST',
+            size: '20px'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: true
+        },
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true,
-                    max: 1
+                    suggestedMax: 1,
+                    beginAtZero:true
                 }
             }]
-        }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
     }
 });
+
+var pctx = document.getElementById("percentileChart").getContext('2d');
+
+pdata = {
+    datasets: [{
+        data: [percentile, 100-percentile],
+        backgroundColor: [
+            'rgba(255, 74, 75, 1)'
+        ],
+        // borderWidth: 1,
+        // borderColor: 'rgba(220, 60, 100, 1)',
+    }],
+};
+var myDoughnutChart = new Chart(pctx, {
+    type: 'doughnut',
+    data: pdata,
+    options: {
+        // title: {
+        //     display: true,
+        //     // text: 'Percentile'
+        // },
+        responsive:true,
+        maintainAspectRatio: false,
+    }
+});
+
+var sdctx = document.getElementById("sdChart").getContext('2d')
+var sdData = {
+    datasets: [{
+        data: [{x:sd, y:1}],
+        backgroundColor: [
+            'rgba(255, 74, 75, 1)'
+        ],
+        // borderWidth: 1,
+        // borderColor: 'rgba(220, 60, 100, 1)',
+    }]
+}
+var sdChart = new Chart(sdctx, {
+    type: 'scatter',
+    data: sdData,
+    options: {
+        // title: {
+        //     display: true,
+        //     text: 'Standard Deviation'
+        // },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    max: 3,
+                    min: -3,
+                    beginAtZero:true
+                }
+            }]
+        },
+        maintainAspectRatio: false,
+        responsive:true
+    }
+})
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,4 +1,26 @@
-var report = JSON.parse(rep);
+var reports = JSON.parse(rep);
+var report = reports.benchmark
+var report2 = reports.benchmark2
+var stats = reports.statistics
+
+var compareTwo = reports.benchmark2 != undefined
+console.log(compareTwo)
+console.log(reports)
+$(document).ready(function(){
+    addButtons(report)
+    // load first available framework
+    let fw = Object.keys(report.frameworks)
+    fwClick(fw[0])
+
+    myChart.data.datasets[0].label = "Benchmark " + report.id
+    if(report2 != null) {
+        myChart.data.datasets[1].label = "Benchmark " + report2.id
+    } else {
+        myChart.data.datasets[1].label = "Average"
+    }
+
+    myChart.update()
+})
 
 function getFrameworkData(json, framework) {
     let data = []
@@ -20,16 +42,64 @@ function getFrameworkData(json, framework) {
 }
 
 function fwClick(e) {
+    setChartData(e)    
+    computeIcons(e)
+
+    // change button background colors
+    let btns = document.getElementsByClassName("btn")
+
+    for(let i = 0; i < btns.length; i++) {
+        if(btns[i].value === e) {
+            btns[i].style.backgroundColor = '#253042'
+            btns[i].style.color = '#ff5b54'
+            btns[i].onmouseover
+            // btns[i].style. 
+        } else {
+            btns[i].style.backgroundColor = 'inherit'
+            btns[i].style.color = 'white'
+        }
+    }
+}
+
+function setChartData(e) {
     if(report.frameworks[String(e)]) {
-        // console.log(report.frameworks[String(e)])
         let data = getFrameworkData(report.frameworks[String(e)], String(e))
-        // console.log(myChart.data.datasets[0].data)
         myChart.data.labels = data.map(function(d) {return d.name})
         myChart.data.datasets[0].data = data.map(function(d) {return d.value})
-        // console.log(myChart.data.datasets[0].data)
+
+        if(report2 != undefined) {
+            let data2 = getFrameworkData(report2.frameworks[String(e)], String(e))
+            myChart.data.labels = data2.map(function(d) {return d.name})
+            myChart.data.datasets[1].data = data2.map(function(d) {return d.value})
+            myChart.data.datasets[1].hidden = false
+        }
+        myChart.options.title.text = e
         myChart.update()
     }
     else {
         console.log("data not available")
+    }
+}
+
+function computeIcons(fw) {
+    console.log(fw)
+}
+
+function displayBenchmark(b) {
+    myChart.data.datasets[b].hidden = !myChart.data.datasets[b].hidden;
+    myChart.update()
+    document.getElementsByClassName("togglebtn")[b].style.backgroundColor = myChart.data.datasets[b].hidden ? '#364554' :'#9CABBA'
+}
+
+function addButtons(report) {
+    var keys = Object.keys(report.frameworks)
+
+    for(var i = 0; i < keys.length; i++) {
+        var $but = $('<button onclick="fwClick(value)" value="' + keys[i] + '" class="btn" type="button">'+ keys[i] + ' </button>')
+        // add background color change on hover
+        $but.hover(function() {
+
+        })
+        $but.appendTo((".btn-group"))
     }
 }
