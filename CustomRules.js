63b31d7230317b473e77bf6be854825f88b9a7b7
Joe Durand <joe.durand3@gmail.com>
2018-03-05 11:55:48 -0800

    comcast box

--- CustomRules.js
+++ CustomRules.js
@@ -3,7 +3,7 @@ module.exports = [
     {
         "contains" : "Comcast",
         "set" : {
-            "ua_type" : "Comcast STBSTB",
+            "ua_type" : "Comcast STB",
             "os_name" : "Comcast",
             "os_version" : null,
             "browser_name" : "Comcast"
